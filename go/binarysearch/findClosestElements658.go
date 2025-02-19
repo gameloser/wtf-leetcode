5f63a34e27883ae2a96c634a81a0f8c14e6a3e90
gameloser <ted.zgy@email.com>
2025-02-20 05:00:41 +0800

    update

--- go/binarysearch/findClosestElements658.go
+++ go/binarysearch/findClosestElements658.go
@@ -6,18 +6,18 @@ import (
 )
 
 func findClosestElements(arr []int, k int, x int) []int {
-	p := lowerBoundOpen(arr, x)
-	// (l, r]
-	l, r := p, p
-	for ; k > 0; k-- {
+	p := findClosest(arr, x)
+	// (l, r)
+	l, r := p-1, p+1
+	for ; k-1 > 0; k-- {
 		// |a - x| < |b - x| 或者
 		// |a - x| == |b - x| 且 a < b
 		// => |a - x| <= |b - x| 且 a < b
-		if r >= len(arr)-1 && l > 0 {
+		if r >= len(arr) && l >= 0 {
 			l--
 			continue
 		}
-		if l <= 0 && r < len(arr) {
+		if l < 0 && r <= len(arr) {
 			r++
 			continue
 		}
@@ -27,14 +27,14 @@ func findClosestElements(arr []int, k int, x int) []int {
 			r++
 		}
 	}
-	return arr[l+1 : r+1]
+	return arr[l+1 : r]
 }
 
-func lowerBoundOpen(nums []int, target int) int {
+func findClosest(nums []int, target int) int {
 	l, r := -1, len(nums)
 	for l+1 < r {
 		mid := l + (r-l)/2
-		if nums[mid] >= target {
+		if nums[mid] >= nums[mid+1] {
 			r = mid
 		} else {
 			l = mid
@@ -44,7 +44,9 @@ func lowerBoundOpen(nums []int, target int) int {
 }
 
 func main() {
-	fmt.Println(findClosestElements([]int{1, 2, 3, 4, 5}, 4, 3))
-	fmt.Println(findClosestElements([]int{1, 1, 1, 10, 10, 10}, 1, 9))
+	//fmt.Println(findClosestElements([]int{1, 2, 3, 4, 5}, 4, 3))
+	//fmt.Println(findClosestElements([]int{1, 1, 1, 10, 10, 10}, 1, 9))
+	//fmt.Println(findClosestElements([]int{0, 0, 1, 2, 3, 3, 4, 7, 7, 8}, 3, 5))
+	fmt.Println(findClosest([]int{0, 0, 1, 2, 3, 3, 4, 7, 7, 8}, 5))
 
 }
